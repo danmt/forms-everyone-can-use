748c2b88a70a409395f8e0637a3f2386d5f15183
Daniel Marin <danielarturomt@gmail.com>
2020-07-22 18:00:16 -0300

    add bad form structure

--- apps/site/src/app/bad/bad.component.ts
+++ apps/site/src/app/bad/bad.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'a11y-forms-bad',
   templateUrl: './bad.component.html',
-  styleUrls: ['./bad.component.scss']
+  styleUrls: ['./bad.component.scss'],
 })
 export class BadComponent implements OnInit {
+  form = this.fb.group({
+    firstName: '',
+    lastName: '',
+    age: 18,
+    phone: '',
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  submit() {}
 }
--- apps/site/src/app/bad/bad.module.ts
+++ apps/site/src/app/bad/bad.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BadRoutingModule } from './bad-routing.module';
 import { BadComponent } from './bad.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BadComponent],
-  imports: [
-    CommonModule,
-    BadRoutingModule
-  ]
+  imports: [CommonModule, BadRoutingModule, ReactiveFormsModule],
 })
-export class BadModule { }
+export class BadModule {}
