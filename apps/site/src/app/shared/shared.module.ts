095289ecc87f88082181d2bd0d89913a97babce6
Daniel Marin <danielarturomt@gmail.com>
2020-07-23 16:42:19 -0300

    fix wrong import

--- apps/site/src/app/shared/shared.module.ts
+++ apps/site/src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { COMPONENTS } from '../core/components';
+import { COMPONENTS } from './components';
 
 @NgModule({
 	declarations: [...COMPONENTS],
